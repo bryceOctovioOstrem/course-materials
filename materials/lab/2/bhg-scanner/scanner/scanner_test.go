7903e40a02a7344f3f2ff62561e4fde6038413be
bryce Octovio Ostrem <48968196+bryceOctovioOstrem@users.noreply.github.com>
2022-02-11 17:50:15 -0700

    Update scanner_test.go

--- materials/lab/2/bhg-scanner/scanner/scanner_test.go
+++ materials/lab/2/bhg-scanner/scanner/scanner_test.go
@@ -6,20 +6,22 @@ import (
 
 // THESE TESTS ARE LIKELY TO FAIL IF YOU DO NOT CHANGE HOW the worker connects (e.g., you should use DialTimeout)
 func TestOpenPort(t *testing.T){
-
-    got := PortScanner() // Currently function returns only number of open ports
+	valuesToTest := []int{2,2}
+    open, _ := PortScanner( valuesToTest ) // Currently function returns only number of open ports
     want := 2 // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns 
 	          //consider what would happen if you parameterize the portscanner address and ports to scan
 
-    if got != want {
-        t.Errorf("got %d, wanted %d", got, want)
+    if open != want {
+        t.Errorf("got %d, wanted %d", open, want)
     }
 }
 
 func TestTotalPortsScanned(t *testing.T){
 	// THIS TEST WILL FAIL - YOU MUST MODIFY THE OUTPUT OF PortScanner()
-
-    got := PortScanner() // Currently function returns only number of open ports
+	var portsArray []int 
+	portsArray = []int{34,87,490,616,65,839,902,200,891,409,10}
+    open, close := PortScanner(portsArray) // Currently function returns number of ports
+	got:= open+close
     want := 1024 // default value; consider what would happen if you parameterize the portscanner ports to scan
 
     if got != want {
